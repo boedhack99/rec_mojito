303c45d98a5a653f528532d61c801e778193f3fb
boedhack99 <boedhack@gmail.com>
2021-05-31 01:48:37 +0700

    need kernel not Image

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,7 +92,7 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.fastboot=1
 #KERNEL_LD := LD=ld.lld
 #BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 #TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 #TARGET_COMPILE_WITH_MSM_KERNEL := true
 #TARGET_KERNEL_CLANG_COMPILE := true

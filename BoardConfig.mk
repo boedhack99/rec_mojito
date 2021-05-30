dc170fd2fa02d4f3299fad5c93c3f4fc382df422
boedhack99 <boedhack@gmail.com>
2021-05-31 02:53:37 +0700

    Now build kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,10 +92,12 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.fastboot=1
 #KERNEL_LD := LD=ld.lld
 #BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+#TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 #TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
-#TARGET_COMPILE_WITH_MSM_KERNEL := true
-#TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := mojito_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/mojito
 
 # Avb
 BOARD_AVB_ENABLE := true

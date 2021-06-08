bcbe6b93632321b186a005d0c5328b5f58a4cfd2
boedhack99 <boedhack@gmail.com>
2021-06-08 19:51:39 +0700

    hardware/qcom-caf/bootctrl namesoongs

--- device.mk
+++ device.mk
@@ -31,7 +31,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/qcom-caf/bootctrl
 
 # Ramdisk
 PRODUCT_COPY_FILES += \

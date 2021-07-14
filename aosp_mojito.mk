cae03ea867038c4d3f237cf2283c1325e3fdf09e
boedhack99 <boedhack@gmail.com>
2021-07-14 19:05:46 +0700

    AOSP Pruduct set

--- aosp_mojito.mk
+++ aosp_mojito.mk
@@ -1,20 +1,10 @@
-#
-# Copyright (C) 2021 The PixelExperience Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := twrp_mojito
+PRODUCT_NAME := aosp_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
